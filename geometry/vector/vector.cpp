6bcb2c0fbcf6bd435050fb10a779c58580dd8397
blue-stone <qingshiji96@gmail.com>
2025-05-22 23:53:50 +0800

    add vector

---
+++ geometry/vector/vector.cpp
@@ -0,0 +1,9 @@
+
+#include "vector.h"
+
+namespace bmath
+{
+namespace geometry
+{
+} // namespace geometry
+} // namespace bmath
---
+++ geometry/vector/vector.h
@@ -0,0 +1,12 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include "geometry/common.h"
+
+namespace bmath
+{
+namespace geometry
+{
+} // namespace geometry
+} // namespace bmath
+#endif // VECTOR_H
